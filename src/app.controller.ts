3cf021aadd18515fae5f6789f64aeddf5f6f7dfd
ingdantev <ingdantev@gmail.com>
2025-04-16 21:02:31 +0000

    download file ok and base64 ok

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,27 +1,44 @@
-import { Controller, Get, NotFoundException, Res } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, NotFoundException, Param, Res } from '@nestjs/common';
+import { AppService, fixPathAudio } from './app.service';
 import { Response } from 'express';
 import * as fs from 'fs'
 import * as path from 'path';
 
 @Controller()
 export class AppController {
+  private filePath = ''; // Ruta a la carpeta src
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
-  @Get('soun')
-  async sound(@Res() res: Response
-  ){
-    await this.appService.sounService()
+  @Get('download/:filename')
+  async downloadFile(
+    @Param('filename') filename: string,
+    @Res() res: Response,
+  ): Promise<void> {
+    this.filePath = fixPathAudio(filename);
     
-    const filePath = path.join(process.cwd(),'/','dist/src', 'output.mp3');
 
     try {
-      await fs.promises.access(filePath); // Verifica si el archivo existe
-      res.sendFile(filePath);
+      await fs.promises.access(this.filePath); // Verifica si el archivo existe
+      res.sendFile(this.filePath);
+    } catch (error) {
+      throw new NotFoundException(`El archivo ${filename} no se encontró.`);
+    }
+  }
+
+  @Get('soun')
+  async sound(){
+    
+    
+    //const filePath = path.join(process.cwd(),'/','dist/src', 'output.mp3');
+
+    try {
+      //await fs.promises.access(filePath); // Verifica si el archivo existe
+      //res.sendFile(filePath);
+      return this.appService.sounService()
     } catch (error) {
       throw new NotFoundException(`El archivo output.mp3 no se encontró.`);
     }
--- src/app.service.ts
+++ src/app.service.ts
@@ -8,72 +8,67 @@ import * as path from 'path';
 
 @Injectable()
 export class AppService {
-  //private client: TextToSpeechClient
-
-  
+   
   constructor(
    
     private readonly configService: ConfigService,
-  ){
-    
-    
-  }
+  ){  }
   
+
   getHello(): string {
     return 'Hello World!';
   }
   
-  async sounService(){
+  async sounService() {
     const client = new textToSpeech.TextToSpeechClient({
       credentials: {
-            type: this.configService.get('TYPE'),
-            client_email: this.configService.get('CLIENT_EMAIL'),
-            private_key: process.env.PRIVATE_KEY,
-            private_key_id: this.configService.get('PRIVATE_KEY_ID'),
-            project_id: this.configService.get('PROJECT_ID'),
-            client_id: this.configService.get('CLIENT_ID'),
-            universe_domain: this.configService.get('UNIVERSE_DOMAIN'),
+        type: this.configService.get('TYPE'),
+        client_email: this.configService.get('CLIENT_EMAIL'),
+        private_key: process.env.PRIVATE_KEY,
+        private_key_id: this.configService.get('PRIVATE_KEY_ID'),
+        project_id: this.configService.get('PROJECT_ID'),
+        client_id: this.configService.get('CLIENT_ID'),
+        universe_domain: this.configService.get('UNIVERSE_DOMAIN'),
       }
     });
-    
+
     // The text to synthesize
-  const text = 'Hola , Mundo!';
+    const text = 'Hola , Mundo!';
 
-  // Construct the request
-  const request = {
-    input: {text: text},
-    // Select the language and SSML voice gender (optional)
-    voice: {
-      languageCode: 'es-ES', 
-      ssmlGender: 'Female'},
-    // select the type of audio encoding
-    audioConfig: {audioEncoding: 'MP3'},
-  };
-  try {
-    const respuesta = async()=>{
-      const [myrespuesta] = await client.synthesizeSpeech(request as any)
-      const filePath = path.join(process.cwd(),'/','dist/src', 'output.mp3');
-      await fs.promises.writeFile(filePath, myrespuesta.audioContent, 'binary'); 
+    // Construct the request
+    const request = {
+      input: { text: text },
+      // Select the language and SSML voice gender (optional)
+      voice: {
+        languageCode: 'es-ES',
+        ssmlGender: 'Male'
+      },
+      // select the type of audio encoding
+      audioConfig: { audioEncoding: 'MP3' },
+    };
+    // const filePath = path.join(process.cwd(),'/','dist/src', 'output.mp3');
 
-    }
-    await respuesta() 
-    
-    
-    
+    const [myrespuesta] = await client.synthesizeSpeech(request as any)
+    fs.writeFileSync(fixPathAudio('output.mp3'),myrespuesta.audioContent)
+    const uno = fs.readdirSync(path.join(process.cwd(),'/','dist/src'))
+    console.log("con el output.mp3",uno)
 
-  } catch (error) {
-    console.error("error",error)
-    
-  };
-    
-  
-					
-					
-          
-       
-         
-          
-  
+    return { "mybase64audio": Buffer.from(myrespuesta.audioContent).toString('base64') }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   }
 }
